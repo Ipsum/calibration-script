74c1e177709e18da128cd34e76debb337ae14ba3
Ipsum <dtyler59@gmail.com>
2010-03-18 13:24:12 -0400

    fixed input regex

--- DB stuff/precal.py
+++ DB stuff/precal.py
@@ -7,12 +7,12 @@ By David Tyler
 for Clark Solution
 '''
 
-import os
-import re
-import sqlite3
+import os, re, sqlite3
 import ConfigParser
 
+from PyQt4 import QtCore, QtGui
 from datetime import date
+from pcui import *
 
 dbpath = "ClarkSonic.db"
 
@@ -49,37 +49,37 @@ again = 'y'
 
 while(again == 'y'):
     so = raw_input('Sales Order Number: ')
-    patt = re.compile('[^S0-9].')
+    patt = re.compile('[^S0-9]*')
     while(patt.search(so)):
         so = raw_input('Invalid SO#, retry: ')
 
     customer = raw_input('Customer name: ')
-    patt = re.compile('[^A-Za-z ].')
+    patt = re.compile('[^A-Za-z ]*')
     while(patt.search(customer)):
         customer = raw_input('Invalid customer name, retry: ')
         
     castingSn = raw_input('Casting Serial Number: ')
-    patt = re.compile('[^0-9].')
+    patt = re.compile('[^0-9]*')
     while(patt.search(castingSn)):
         castingSn = raw_input('Invalid Casting SN, retry: ')
         
     pcbSn = raw_input('PCB Serial Number: ')
-    patt = re.compile('[^0-9].')
+    patt = re.compile('[^0-9]*')
     while(patt.search(pcbSn)):
         pcbSn = raw_input('Invalid PCB SN, retry: ')
         
     sizeInd = raw_input('Size Index: ')
-    patt = re.compile('[^0-7].')
+    patt = re.compile('[^0-7]*')
     while(patt.search(sizeInd)):
         sizeInd = raw_input('Invalid Size Index, valid entry 0-7: ')
         
     fe = raw_input('First Echo(mV): ')
-    patt = re.compile('[^0-9].')
+    patt = re.compile('[^0-9]*')
     while(patt.search(fe)):
         fe = raw_input('Invalid First Echo, retry: ')
         
     dz = raw_input('Dz: ')
-    patt = re.compile('[^0-9.].')
+    patt = re.compile('[^0-9.]*')
     while(patt.search(dz)):
         dz = raw_input('Invalid Dz, retry: ')
         
